bfa16b8a5105ffdfe046cfbf579bce4f5ff40d63
Luis <luis.beltran@itcelaya.edu.mx>
2020-10-04 19:41:45 +0000

    Add project files.

---
+++ Module01Forms/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Module01Forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.txtInterestRate = new System.Windows.Forms.TextBox();
+            this.txtBalance = new System.Windows.Forms.TextBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnDeposit = new System.Windows.Forms.Button();
+            this.btnWithdraw = new System.Windows.Forms.Button();
+            this.btnAddInterest = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Account Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Account Interest Rate";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(460, 276);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Account Balance";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(182, 38);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(134, 20);
+            this.txtNumber.TabIndex = 3;
+            // 
+            // txtInterestRate
+            // 
+            this.txtInterestRate.Location = new System.Drawing.Point(182, 85);
+            this.txtInterestRate.Name = "txtInterestRate";
+            this.txtInterestRate.Size = new System.Drawing.Size(134, 20);
+            this.txtInterestRate.TabIndex = 4;
+            // 
+            // txtBalance
+            // 
+            this.txtBalance.Enabled = false;
+            this.txtBalance.Location = new System.Drawing.Point(604, 273);
+            this.txtBalance.Name = "txtBalance";
+            this.txtBalance.Size = new System.Drawing.Size(134, 20);
+            this.txtBalance.TabIndex = 5;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(92, 134);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(134, 23);
+            this.btnCreate.TabIndex = 6;
+            this.btnCreate.Text = "Create Account";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Amount";
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Location = new System.Drawing.Point(141, 46);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(134, 20);
+            this.txtAmount.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAddInterest);
+            this.groupBox1.Controls.Add(this.btnWithdraw);
+            this.groupBox1.Controls.Add(this.btnDeposit);
+            this.groupBox1.Controls.Add(this.txtAmount);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(41, 193);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(352, 162);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Operations";
+            // 
+            // btnDeposit
+            // 
+            this.btnDeposit.Location = new System.Drawing.Point(9, 96);
+            this.btnDeposit.Name = "btnDeposit";
+            this.btnDeposit.Size = new System.Drawing.Size(75, 23);
+            this.btnDeposit.TabIndex = 9;
+            this.btnDeposit.Text = "Deposit";
+            this.btnDeposit.UseVisualStyleBackColor = true;
+            this.btnDeposit.Click += new System.EventHandler(this.btnDeposit_Click);
+            // 
+            // btnWithdraw
+            // 
+            this.btnWithdraw.Location = new System.Drawing.Point(105, 96);
+            this.btnWithdraw.Name = "btnWithdraw";
+            this.btnWithdraw.Size = new System.Drawing.Size(75, 23);
+            this.btnWithdraw.TabIndex = 10;
+            this.btnWithdraw.Text = "Withdraw";
+            this.btnWithdraw.UseVisualStyleBackColor = true;
+            this.btnWithdraw.Click += new System.EventHandler(this.btnWithdraw_Click);
+            // 
+            // btnAddInterest
+            // 
+            this.btnAddInterest.Location = new System.Drawing.Point(200, 96);
+            this.btnAddInterest.Name = "btnAddInterest";
+            this.btnAddInterest.Size = new System.Drawing.Size(75, 23);
+            this.btnAddInterest.TabIndex = 11;
+            this.btnAddInterest.Text = "Add Interest";
+            this.btnAddInterest.UseVisualStyleBackColor = true;
+            this.btnAddInterest.Click += new System.EventHandler(this.btnAddInterest_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.txtBalance);
+            this.Controls.Add(this.txtInterestRate);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Account System";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.TextBox txtInterestRate;
+        private System.Windows.Forms.TextBox txtBalance;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnWithdraw;
+        private System.Windows.Forms.Button btnDeposit;
+        private System.Windows.Forms.Button btnAddInterest;
+    }
+}
+
---
+++ Module01Forms/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Module01Forms
+{
+    public partial class Form1 : Form
+    {
+        // The object we are going to use
+        BankAccount savings;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // When clicking this button, call the constructor and create a BankAccount object 
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            // Get the input values from textboxes
+            string number = txtNumber.Text;
+            float interestRate = float.Parse(txtInterestRate.Text);
+
+            // Create the BankAccount object
+            savings = new BankAccount(number, 0, interestRate);
+
+            // Show the Balance in the specific textbox
+            txtBalance.Text = savings.Balance.ToString("C");
+
+            // Disable the textboxes and button
+            txtNumber.Enabled = false;
+            txtInterestRate.Enabled = false;
+            btnCreate.Enabled = false;
+
+            // Display a message to the user
+            MessageBox.Show("The account has been created!");
+        }
+
+        // When clicking this button, call the Deposit method
+        private void btnDeposit_Click(object sender, EventArgs e)
+        {
+            // Get the input value from textbox
+            float amount = float.Parse(txtAmount.Text);
+
+            // Call the Deposit method from BankAccount
+            savings.Deposit(amount);
+
+            // Show the new Balance in the specific textbox
+            txtBalance.Text = savings.Balance.ToString("C");
+
+            // Display a message to the user
+            MessageBox.Show("The account balance has been updated!");
+        }
+
+        // When clicking this button, call the Withdraw method
+        private void btnWithdraw_Click(object sender, EventArgs e)
+        {
+            // Get the input value from textbox
+            float amount = float.Parse(txtAmount.Text);
+
+            // Call the Withdraw method from BankAccount
+            savings.Withdraw(amount);
+
+            // Show the new Balance in the specific textbox
+            txtBalance.Text = savings.Balance.ToString("C");
+
+            // Display a message to the user
+            MessageBox.Show("The account balance has been updated!");
+        }
+
+        private void btnAddInterest_Click(object sender, EventArgs e)
+        {
+            // Call the AddInterest method from BankAccount
+            savings.AddInterest();
+
+            // Show the new Balance in the specific textbox
+            txtBalance.Text = savings.Balance.ToString("C");
+
+            // Display a message to the user
+            MessageBox.Show("The account balance has been updated!");
+        }
+    }
+}
